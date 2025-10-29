faa58d717e4e8acca845dea6f7a6ceb54fdecd4c
Nicolas <nicolas.schek@gmail.com>
2025-10-29 11:21:50 -0300

    Fix: Agregar extensión .tsx al import de App

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
